50b007d252d065ef78f4b4999d751e0fe06e5022
callu <c4043156@newcastle.ac.uk>
2025-03-01 03:19:26 +0000

    INITIAL COMMIT
    - completed the boiler code to add a range of functionalities, from reading and writing to save files, to add and removing items from a list.
    
    NEXT COMMIT SHOULD INCLUDE:
    
    - Startingt to implement a GUI.

--- shopList.java
+++ shopList.java
@@ -1,39 +1,83 @@
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class shopList {
-List <String> shoppingList = new ArrayList<>();
-Map <String,Double> food = new HashMap<>();
+    List <String> shoppingList;
+    Map <String,Double> food = new HashMap<>();
 
 
     public shopList(List<String> shoppingList) {
         this.shoppingList = shoppingList;
     }
 
-    public static void addToList() {
+    public static void addToList(String givenItem,Map<String,Double> food,List<String> shoppingList) {
+        if (food.containsKey(givenItem)) {
+            shoppingList.add(givenItem);
+        }
     }
 
-    public static void removeFromlist() {
+    public static void removeFromlist(String givenItem,List<String> shoppingList) {
+        if (shoppingList.contains(givenItem)) {
+            shoppingList.remove(givenItem);
+        }
     }
 
-    public static void calculatePrice() {
+    public static double calculatePrice(Map<String,Double> food,List<String> shoppingList) {
+        double price = 0.0;
+
+        for (String string:shoppingList) {
+            price += food.get(string);
+        }
+        return price;
     }
 
-    public static void listAllItems() {
+    public static void listAllItems(Map<String,Double> food) {
+        for (Map.Entry<String,Double> Map:food.entrySet()) {
+            System.out.println("Item:  " + Map.getKey() + "Price:  " + Map.getValue());
+        }
     }
 
-    public static void createDictionary() {
+    public static Map<String, Double> createDictionary(String saveFile, Map<String,Double> food) {
+        Scanner reader = new Scanner(saveFile);
+
+        while (reader.hasNextLine()) {
+            String[] foodData = (reader.nextLine()).split(":");
+            String foodName = foodData[0];
+            Double foodPrice = Double.parseDouble(foodData[1]);
+            food.put(foodName,foodPrice);
+        }
+
+        reader.close();
+        return food;
     }
 
-    public static void saveToFile() {
+    public static void saveToFile(List<String> shoppingList,String saveFile) throws IOException {
+        FileWriter writer = new FileWriter(String.format("%.txt",saveFile));
+
+        for (String string:shoppingList) {
+            writer.write(string);
+        }
+
+        writer.close();
+        shoppingList.clear();
     }
 
-    public static void loadFile() {
+    public static void loadFile(List<String> shoppingList,String saveFile) {
+        Scanner reader = new Scanner(saveFile);
+
+        while (reader.hasNextLine()) {
+            shoppingList.add(reader.nextLine());
+        }
+
+        reader.close();
     }
 
     public static void exit() {
+        System.exit(0);
     }
 
 }
