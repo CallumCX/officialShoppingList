44a672c1347836fc36d593acd4488db6d14bcd07
callu <c4043156@newcastle.ac.uk>
2025-03-01 02:06:07 +0000

    INITIAL COMMIT
    - created boiler plating

---
+++ shopList.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class shopList {
+List <String> shoppingList = new ArrayList<>();
+Map <String,Double> food = new HashMap<>();
+
+
+    public shopList(List<String> shoppingList) {
+        this.shoppingList = shoppingList;
+    }
+
+    public static void addToList() {
+    }
+
+    public static void removeFromlist() {
+    }
+
+    public static void calculatePrice() {
+    }
+
+    public static void listAllItems() {
+    }
+
+    public static void createDictionary() {
+    }
+
+    public static void saveToFile() {
+    }
+
+    public static void loadFile() {
+    }
+
+    public static void exit() {
+    }
+
+}
